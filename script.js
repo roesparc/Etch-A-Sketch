4452d22cb1f9ae6d06d8c8332f8e8b67e9708f03
rolastic <rockoesp@hotmail.com>
2022-08-19 16:37:19 -0500

    Add grid

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const body = document.querySelector('body')
+body.classList.add('body')
 
 const gridContainer = document.createElement('div')
+gridContainer.classList.add('gridContainer')
 
 for (let i = 0; i < 256; i++) {
     const grid = document.createElement('div')
+    grid.classList.add('grid')
     gridContainer.appendChild(grid)
 }
 
