33240b01212a94d5d293d4eaf38190b1fa682cf4
rolastic <rockoesp@hotmail.com>
2022-08-20 19:56:49 -0500

    Add button to change grid size Button to the top of the screen that sends the user a popup asking for the number of squares per side for the new grid.

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
-const body = document.querySelector('body')
-body.classList.add('body')
+const body = document.querySelector('body');
 
-const gridContainer = document.createElement('div')
-gridContainer.classList.add('gridContainer')
+const gridContainer = document.querySelector('.gridContainer');
 
-for (let i = 0; i < 256; i++) {
-    const grid = document.createElement('div')
-    grid.classList.add('grid')
-    gridContainer.appendChild(grid)
+const gridContainerBtn = document.createElement('div');
+gridContainerBtn.classList.add('gridContainer');
+
+for (let i = 0; i < 16*16; i++) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    gridContainer.appendChild(grid);
 }
 
-body.appendChild(gridContainer)
-
-const gridDiv = document.querySelectorAll('.grid')
-
+const gridDiv = document.querySelectorAll('.grid');
 gridDiv.forEach(div => div.addEventListener('mouseover', () => {
-    div.style.backgroundColor = 'red'
-}))
+    div.style.backgroundColor = 'red';
+}));
+
+function clickBtn() {
+    const input = prompt ('Enter the New Grid Size (Max 100)');
+
+    if (input === null) return;
+    else if (input < 1 || input > 100 || isNaN(input)) {
+        alert('Invalid Number');
+        return;
+    }
+
+    body.removeChild(gridContainer);
+
+    for (let i = 0; i < input*input; i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        gridContainerBtn.appendChild(grid);
+    }
+
+    body.appendChild(gridContainerBtn);
+
+    const gridDiv = document.querySelectorAll('.grid');
+    gridDiv.forEach(div => {
+        const divSize = 16 / input * 30;
+        div.style.width = `${divSize}px`;
+        div.style.height = `${divSize}px`;
+    });
+    gridDiv.forEach(div => div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'red';
+    }));
+}
